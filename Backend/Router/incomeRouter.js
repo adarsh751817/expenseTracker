8bf40e8296ab59ef1e3d48d5c890460ec34c99d4
adarsh751817 <145890098+adarsh751817@users.noreply.github.com>
2025-08-15 22:14:17 +0530

    Update incomeRouter.js

--- Backend/Router/incomeRouter.js
+++ Backend/Router/incomeRouter.js
@@ -1,22 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const { authMiddleware } = require("../middleware"); // ✅ make sure it's "middleware" not "middleWare"
-
-// const {
-//   createIncome,
-//   getIncomes,
-//   updateIncome,
-//   deleteIncome,
-//   myIncome
-// } = require("../controlers/incomeControler"); // ✅ also correct "controlers" → "controllers" if needed
-
-// // ✅ Use authMiddleware consistently
-// router.post("/", authMiddleware, createIncome);
-// router.get("/all_income", authMiddleware, getIncomes);
-// router.put("/:id", authMiddleware, updateIncome);
-// router.delete("/:id", authMiddleware, deleteIncome);
-// router.get("/my_income", authMiddleware, myIncome);    
-// module.exports = router;
 
 
 const express = require("express");
